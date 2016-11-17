194b207e37b2434cf8f1e1230db3693a446c96a0
admin <sanya_90_206@mail.ru>
2016-11-17 22:12:17 +0200

    add history js

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function () {
         url: 'https://jsonplaceholder.typicode.com/users ',
         type: 'GET',
         success: function (response) {
-            // console.log('response');
+            console.log(history);
             // console.log(response);
             for (var i = 0; i < response.length; i++) {
                 // console.log(response[i].id)
@@ -37,6 +37,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var table = document.querySelector('table');
     table.addEventListener('click', function(event) {
         // event.stopPropagation();
+
     var target = event.target,
         dataArticles = target.getAttribute('data-articles');
         if(target.className == 'articles') {
@@ -44,19 +45,22 @@ document.addEventListener('DOMContentLoaded', function () {
                 url: 'https://jsonplaceholder.typicode.com/posts?userId='+dataArticles,
                 type: 'GET',
                 success: function (response) {
+                    var mianBody = document.querySelector('body');
                     var parentTr = target.parentNode.parentNode;
                     var parentId = parentTr.id;
+                    console.log(mianBody)
+                    history.pushState('indexfoo', "page 2", "#user"+parentId);
                     for (var i = 0; i < response.length; i++) {
-                        var tmpArticles = '<tr class="comment-tr">' +
+                        var tmpArticles = '<div class="container"><tr class="comment-tr">' +
                             '<td colspan="3" class="block-sm">' +
                             '<div class="block-title"><b>' + response[i].id + ' </b>' + response[i].title + '</div>' +
                             ' <div class="block-description">' + response[i].body + '</div>' +
                             '<button class="comment" data-comment="' + response[i].id + '">Получить комментарий</button>'
                         ' </td>' +
-                        ' </tr>';
+                        ' </tr></div>';
                         if (response[i].userId == parentId) {
                             // console.log(response[i])
-                            parentTr.insertAdjacentHTML('beforebegin', tmpArticles);
+                            mianBody.innerHTML += tmpArticles;
                         }
 
                     }
