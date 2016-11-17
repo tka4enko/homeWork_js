300e4e58f14e34f2281d709effa450f842032c5a
sanya_90_2016 <sanya_90_2016@mail.ru>
2016-11-17 15:30:51 +0200

    update

--- js/main.js
+++ js/main.js
@@ -9,7 +9,6 @@ document.addEventListener('DOMContentLoaded', function () {
     tBody = document.querySelector('tbody');
     tPhone = document.getElementsByClassName('phone');
     tName = document.getElementsByClassName('name');
-    console.log(tBody)
     function loadAjax(param) {
         var xhr = new XMLHttpRequest();
         xhr.open(param.type, param.url);
@@ -28,9 +27,9 @@ document.addEventListener('DOMContentLoaded', function () {
             // console.log(response);
             for (var i = 0; i < response.length; i++) {
                 // console.log(response[i].id)
-                var tmp = '<tr >' +
+                var tmp = '<tr id ="'+response[i].id+'" >' +
                     '<td class="name">' + response[i].name + '</td>' +
-                    '<td class="phone">' + response[i].phone + '</td><td><button id="articles-'+response[i].id+'" type="submit">Отобразить статьи</button></td></tr>'
+                    '<td class="phone">' + response[i].phone + '</td><td><button id="articles" type="submit">Отобразить статьи</button></td></tr>'
                       tBody.innerHTML += tmp
             }
         }
@@ -39,38 +38,40 @@ document.addEventListener('DOMContentLoaded', function () {
     table.addEventListener('click', function(event) {
         // event.stopPropagation();
         var target = event.target;
-        console.log(event)
-        if(target.id == 'articles-1'){
-          loadAjax({
-            url: 'https://jsonplaceholder.typicode.com/posts',
-            type: 'GET',
-            success: function (response) {
-                var parentTr = target.parentNode.parentNode;
-                for (var i = 0; i < response.length; i++) {
-                    var tmpArticles ='<tr class="comment-tr">' +
-                                    '<td colspan="2" class="block-sm">'+
-                                        '<div class="block-title"><b>'+i+' </b>' +response[i].title+'</div>'+
-                                       ' <div class="block-description">'+response[i].body+'</div>'+
-                                        '<button class="comment" id="'+response[i].id+'">Получить комментарий</button>'
-                                   ' </td>'+
-                               ' </tr>';
-                    if(response[i].userId == 1){
-                        // console.log(response[i])
-                        parentTr.insertAdjacentHTML('beforebegin', tmpArticles);
-                    }
 
+        if(target.id == 'articles') {
+            loadAjax({
+                url: 'https://jsonplaceholder.typicode.com/posts',
+                type: 'GET',
+                success: function (response) {
+                    var parentTr = target.parentNode.parentNode;
+                    var parentId = parentTr.id;
+                    for (var i = 0; i < response.length; i++) {
+                        var tmpArticles = '<tr class="comment-tr">' +
+                            '<td colspan="3" class="block-sm">' +
+                            '<div class="block-title"><b>' + response[i].id + ' </b>' + response[i].title + '</div>' +
+                            ' <div class="block-description">' + response[i].body + '</div>' +
+                            '<button class="comment" id="' + response[i].id + '">Получить комментарий</button>'
+                        ' </td>' +
+                        ' </tr>';
+                        if (response[i].userId == parentId) {
+                            // console.log(response[i])
+                            parentTr.insertAdjacentHTML('beforebegin', tmpArticles);
+                        }
+
+                    }
                 }
-            }
-        });
+            })
         }
-        else if(target.className == 'comment' && target.id == '3' ){
+        else if(target.className == 'comment' ){
             loadAjax({
                 url: 'https://jsonplaceholder.typicode.com/comments',
                 type: 'GET',
                 success: function (response) {
-                    console.log(response);
+                    // console.log(response);
                     var parentCommet = target.parentNode;
-                    console.log(parentCommet);
+                    var parentCommetId = target.parentNode;
+
                     for (var i = 0; i < response.length; i++) {
                         // console.log(response[i])
                         var tmpComment ='<div class="comment-block id-'+response[i].id+'">' +
@@ -78,8 +79,8 @@ document.addEventListener('DOMContentLoaded', function () {
                                            ' <div class="comment-description"><b>Content:</b>'+response[i].body+'</div>'+
                                            ' <div class="comment-email"><b>Email:</b>'+response[i].email+'</div>'+
                                    ' </div>';
-                        if(response[i].postId == 3){
-                            // console.log(response[i])
+                        if(response[i].postId == target.id){
+                            console.log(response[i])
                             parentCommet.insertAdjacentHTML('afterbegin', tmpComment);
                         }
 
